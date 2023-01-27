8a235e2cca94bbb921964725fe0c8e034a79e2c8
Aidar15 <aidar.kafizov@gmail.com>
2023-01-27 18:36:43 +0300

    fix:(common): make changes

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('.social-blog__photo').click(function(event){
+        var i_path = $(this).attr('src');
+        $('.social-blog').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
+        $('#magnify').css({
+            left: ($(document).width() - $('#magnify').outerWidth())/2,
+            top: ($(window).height() - $('#magnify').outerHeight())/2
+        });
+        $('#overlay, #magnify').fadeIn('fast');
+    });
+    $('.social-blog').on('click', '#close-popup, #overlay', function(event) {
+        event.preventDefault();
+        $('#overlay, #magnify').fadeOut('fast', function() {
+        $('#close-popup, #magnify, #overlay').remove();
+        });
+    });
+});
